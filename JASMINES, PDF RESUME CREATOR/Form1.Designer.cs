c684b50e21ea9e2838b99c01d58fcc4d24880f05
John Lloyd A. Jasmines <jasminesj37@gmail.com>
2022-07-13 12:39:45 +0800

    Add Generate Button

--- JASMINES, PDF RESUME CREATOR/Form1.Designer.cs
+++ JASMINES, PDF RESUME CREATOR/Form1.Designer.cs
@@ -30,24 +30,38 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(F1));
             this.LblTITLE = new System.Windows.Forms.Label();
+            this.ButtGEN = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // LblTITLE
             // 
             this.LblTITLE.AutoSize = true;
-            this.LblTITLE.Font = new System.Drawing.Font("Britannic Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.LblTITLE.Location = new System.Drawing.Point(159, 57);
+            this.LblTITLE.Font = new System.Drawing.Font("Britannic Bold", 18F);
+            this.LblTITLE.Location = new System.Drawing.Point(12, 24);
             this.LblTITLE.Name = "LblTITLE";
             this.LblTITLE.Size = new System.Drawing.Size(157, 27);
             this.LblTITLE.TabIndex = 0;
             this.LblTITLE.Text = "Generate PDF";
             // 
+            // ButtGEN
+            // 
+            this.ButtGEN.BackColor = System.Drawing.Color.Khaki;
+            this.ButtGEN.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ButtGEN.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtGEN.Location = new System.Drawing.Point(302, 254);
+            this.ButtGEN.Name = "ButtGEN";
+            this.ButtGEN.Size = new System.Drawing.Size(109, 38);
+            this.ButtGEN.TabIndex = 1;
+            this.ButtGEN.Text = "Generate";
+            this.ButtGEN.UseVisualStyleBackColor = false;
+            // 
             // F1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkOrange;
-            this.ClientSize = new System.Drawing.Size(509, 366);
+            this.ClientSize = new System.Drawing.Size(436, 317);
+            this.Controls.Add(this.ButtGEN);
             this.Controls.Add(this.LblTITLE);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "F1";
@@ -60,5 +74,6 @@
         #endregion
 
         private Label LblTITLE;
+        private Button ButtGEN;
     }
 }
